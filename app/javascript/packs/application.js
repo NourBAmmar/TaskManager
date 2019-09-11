d018d47b4ba098edada642f163be9d5d4efc128d
NourBAmmar <bammarnour@gmail.com>
2019-09-11 11:20:59 +0200

    second

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-//= require jquery
-//= require jquery-ui
 //= require jquery_ujs
-//= require_tree 
 //= require popper
 //= require bootstrap-sprockets
 
